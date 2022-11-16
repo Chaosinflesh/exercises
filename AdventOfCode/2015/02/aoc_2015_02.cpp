10f1d97a568c4d9d5c4994cabeb0d508cf186560
Neil Bradley <80152378+Chaosinflesh@users.noreply.github.com>
2022-11-16 21:54:38 +1300

    Added AoC 2016.1.zig.

---
+++ AdventOfCode/2015/02/aoc_2015_02.cpp
@@ -0,0 +1,15 @@
+// Advent of Code 2015.2: C++
+//
+// Author: Neil Bradley
+// Copyright: Neil Bradley
+// License: All rights reserved.
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv) {
+
+    std::string 
+
+    return 0;
+}
